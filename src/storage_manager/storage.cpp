7073271c78db2845a80c888bb20b3c3027df59a8
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-04-06 12:09:02 +0800

    - added void function for basic operations

--- src/storage_manager/storage.cpp
+++ src/storage_manager/storage.cpp
@@ -4,21 +4,53 @@
     #include <map>
     #include <filesystem>
     #include <string>
+    using namespace std;
 
 
     void createDirectory(std::string directory_name) {
         
         assert(!std::filesystem::create_directory(directory_name));
-
-        std::filesystem::create_directory("./" + directory_name);
+        std::filesystem::create_directory(directory_name);
 
     }
 
-    void createFile() {
+    void createFile(std::string directory_name, std::string file_name) {
 
-        std::fstream::Init;
+        std::ofstream file(directory_name + "/" + file_name);
+        file.close();
 
+    }
 
+    void selectDirectory(std::string directory_name) {
+        std::fstream directory;
+
+        assert(!std::filesystem::exists(directory_name));
+        
+        directory.open(directory_name, std::ios::in);
+
+        if (directory.is_open()) {
+            std::string directory_items;
+
+            while (std::getline(directory, directory_items)) {
+                std::cout << directory_items << std::endl;
+            }
+        }
+    }
+
+    void selectFile(std::string directory_name, std::string file_name) {
+        std::fstream file;
+
+        assert(!std::filesystem::exists(directory_name + "/" + file_name));
+
+        file.open(directory_name + "/" + file_name, std::ios::in);
+
+        if (file.is_open()) {
+            std::string file_content;
+
+            while (std::getline(file, file_content)) {
+                std::cout << file_content << std::endl;
+            }
+        }
     }
 
     int main() {
