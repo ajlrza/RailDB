ac9a32d84a315a41805b71a1356d2788591a06b3
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-05-18 20:16:57 +0800

    refactor: Convert transactionQueue from fixed array to std::vector
    - Replace fixed-size array (queueNode transactionQueue[50]) with
      std::vector<queueNode> for dynamic memory management
    - Update Enqueue() to use push_back() for dynamic insertion
    - Refactor Dequeue() logic to use vector front/back operations
    - Add #include <vector> header
    - Improves memory flexibility and queue management

--- src/transaction_manager/transaction_manager.cpp
+++ src/transaction_manager/transaction_manager.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <filesystem>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct queueNode {
@@ -11,9 +12,11 @@ struct queueNode {
     std::string query;
 };
 
-
 // Complete queue computer logic and fix shit
 // Queue ver 0.45
+
+
+// Queue for incoming transaction request and serialization
 
 class transactionsQueue {
     public:
@@ -21,38 +24,43 @@ class transactionsQueue {
         queueNode *tailPointer;
         queueNode *headPointer;
         queueNode node;
-        queueNode transactionQueue[50];
+        std::vector<queueNode> transactionQueue;
 
     void Enqueue(queueNode &el) {
         if (this->howMany == 0) {
-           this->transactionQueue[howMany - 1] = el;
-           this->headPointer = &transactionQueue[howMany];
+           this->transactionQueue.push_back(el);
+           this->headPointer = &transactionQueue.back();
            this->tailPointer = nullptr;
            howMany = howMany + 1;
         }
         else if (this->howMany >= 1) {
-            this->transactionQueue[howMany] = el;
-            this->tailPointer = &transactionQueue[howMany];
+            this->headPointer = &transactionQueue.front();
+            this->transactionQueue.push_back(el);
+            this->tailPointer = &transactionQueue.back();
             howMany = howMany + 1;
         }
     }
 
     void Dequeue(queueNode &el) {
        if (this->howMany == 1) {
-            howMany = howMany - 1;
+            howMany = 0;
             this->headPointer = nullptr;
             this->tailPointer = nullptr;
        }
        else if (this->howMany > 1) {
             howMany = howMany - 1;
-            if (howMany == 0) {
+            if (howMany == 0) {             
                 this->headPointer = nullptr;
                 this->tailPointer = nullptr;
             }
             else {
-                this->transactionQueue[howMany - 1] = el;
-                this->headPointer = &transactionQueue[0];
-                this->tailPointer = &transactionQueue[howMany - 1];
+                int index;
+                for (size_t i = 0; i < transactionQueue.size(); ++i) {
+                    index = (i + 1) % transactionQueue.size();
+                    transactionQueue[i] = transactionQueue[index];
+                }
+                this->headPointer = &transactionQueue.front();
+                this->tailPointer = &transactionQueue.back();
             }
        }
     }
