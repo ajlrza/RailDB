f73c9b68b46d452627d8acba5b8cca3dbc63f835
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-05-28 20:26:29 +0800

    Feat: Add the header names in the table_management header file for each function, class, and structs in the table_management.cpp

--- src/table_management/table_management.h
+++ src/table_management/table_management.h
@@ -7,6 +7,72 @@
 #include <filesystem>
 #include <string>
 using namespace std;
-#endif
 
 struct table;
+struct rowData;
+struct colData;
+
+class DataType {
+    public:
+         virtual ~DataType();
+         virtual std::string GetName();
+         virtual int GetSize(); 
+};
+
+class IntDataType: public DataType {
+    public:
+        int integer;
+        IntDataType(int intvalue, int keyword);
+    private:
+        std::string GetName() override;
+        int GetSize() override;
+};
+
+class FloatDataType: public DataType {
+    std::string GetName() override;
+    int GetSize() override;
+};
+
+class DoubleDataType: public DataType {
+    std::string GetName() override;
+    int GetSize() override;
+};
+
+class CharDataType: public DataType {
+    std::string GetName() override;
+    int GetSize() override;
+};
+
+class BoolDataType: public DataType {
+    std::string GetName() override; 
+    int GetSize() override;
+};
+
+class SchemaCreator {
+    public:
+        SchemaCreator(uint32_t schema_name);
+
+        void setStruct();
+
+        void setTable(table Table);
+
+        void setViews(uint32_t views);
+
+        void setStoredP(uint32_t stored_procedures);
+
+        bool storeSchema();
+
+    private:
+        struct Schema;
+};
+
+int randomIDAssigner();
+
+// In creating a directory, maybe it would be automatic or user generated?
+void createTable(std::string name, std::string directory_name);
+
+// Oh yeah THE DIRECTORY itself should be the schema(?)
+void addCol(std::string schema_name, std::string table_name, std::string col_name, std::string data_type);
+
+#endif
+
